4ca705f1a495148b11dbcb9070d15f02717c0759
Gabrielle <76081229+GabrielleKw@users.noreply.github.com>
2022-07-07 11:45:00 -0300

    Add files via upload

---
+++ sistema/Win0/DataaSet.cs
@@ -0,0 +1,30 @@
+﻿namespace win0
+{
+
+
+    partial class DataaSet
+    {
+        partial class tabcadastrotercDataTable
+        {
+        }
+
+        partial class tabcadastrotercNovaDataTable
+        {
+        }
+
+        partial class tablecadastrofuncDataTable
+        {
+        }
+    }
+}
+
+namespace win0.DataaSetTableAdapters
+{
+    partial class tabcadastrotercTableAdapter
+    {
+    }
+
+    public partial class tableterceiroTableAdapter
+    {
+    }
+}
